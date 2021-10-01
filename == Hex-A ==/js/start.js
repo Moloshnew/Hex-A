cf1b8ef0352cdfe6308998dea5659a3bf00ad546
Moloshnew <62329734+Moloshnew@users.noreply.github.com>
2021-10-01 23:37:25 +0300

    Delete == Hex-A == directory

--- == Hex-A ==/js/start.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var canvas, ctx, gl, program;
-    init_canvas();
-});
--- == Hex-A ==/shaders/fragment shader.js
+++
@@ -1,7 +0,0 @@
-precision lowp float;
-
-varying vec3 fragColor;
-
-void main(){
-	gl_FragColor = vec4(fragColor, 1.0);
-}
